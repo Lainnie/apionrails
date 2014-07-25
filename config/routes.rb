10d0e9da5e4398044ece5dbfb47ed7dc1e9f4ca4
lainnie <l.diallo.pro@gmail.com>
2014-07-25 14:25:03 +0000

    Disable crsf protection

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   namespace :api,
-    defaults: { format: :json },
     constraints: { subdomain: 'api' },
-    path: '/' do
+    defaults: { format: :json },
+    path: '' do
       scope module: :v1,
        constraints: ApiConstraints.new(version: 1, default: true) do
         resources :users, only: [:show]
